3de6c468490233b922d771a631aa0df241960840
Carlo <carloxbailey.com>
2020-08-23 21:28:03 -0400

    caller function

---
+++ src/run.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from page_crawler import PageCrawler
+import os
+import argparse
+
+
+def main(args):
+    coords = args.coords
+    coords = zip(coords[0::2], coords[1::2])
+    errors = 0
+    for point in coords:
+        lng = point[1]
+        lat = point[0]
+        url = ('https://www.seamless.com/search?orderMethod=pickup&locationMode=PICKUP'
+               '&facetSet=umamiV2&pageSize=20&hideHateos=true&searchMetrics=true&latitude=%s'
+               '&longitude=%s&preciseLocation=true&sorts=distance&radius=1')
+        pc = PageCrawler(url, headless=True, lat=lat, lng=lng, ll=5, ul=15)
+        data = pc.crawl_page()
+        try:
+            frame = pd.DataFrame(data)
+            dfs.append(frame)
+        except:
+            print("Error for %s location was skipped" % point)
+            errors += 1
+            continue
+        print("process complete! there were %s errors." % errors)
+    pd.concat(dfs).to_csv('%s.csv' % args.filename, index=False)
+
+def parse_arguments():
+    cli = argparse.ArgumentParser()
+    cli.add_argument(
+        "--coords",
+        nargs="*",
+        type=float,
+        default=[40.674792, -73.975310],
+    )
+
+    cli.add_argument(
+        "--filename",
+        nargs="?",
+        type=str,
+        default="seamless_data",
+    )
+
+    args = cli.parse_args()
+    return args
+
+if __name__ == '__main__':
+    job_args = parse_arguments()
+    main(job_args)
